e095bea225d72c7aa847f9b6defdb14a3d17a8e1
xaviguasch <xaviguasch@gmail.com>
2019-12-19 11:28:39 +0100

    added first components and basic styling

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+import Note from './Note'
+import Footer from './Footer'
+
+function App() {
+    return (
+        <div className='App'>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const currentYear = new Date().getFullYear()
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>Copyright © {currentYear}</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
