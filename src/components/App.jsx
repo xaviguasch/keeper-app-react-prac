65a5cedbf83e31a4b7b5664880f901a5ad003661
xaviguasch <xaviguasch@gmail.com>
2019-12-19 18:47:03 +0100

    mapped the Card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,19 @@ function App(props) {
     return (
         <div>
             <h1 className='heading'>My Contacts</h1>
-            <Card
-                name={contacts[0].name}
-                img={contacts[0].imgURL}
-                tel={contacts[0].phone}
-                email={contacts[0].email}
-            />
-            <Card
-                name={contacts[1].name}
-                img={contacts[1].imgURL}
-                tel={contacts[1].phone}
-                email={contacts[1].email}
-            />
-            <Card
-                name={contacts[2].name}
-                img={contacts[2].imgURL}
-                tel={contacts[2].phone}
-                email={contacts[2].email}
-            />
+
+            {contacts.map(contact => {
+                return (
+                    <Card
+                        key={contact.id}
+                        id={contact.id}
+                        name={contact.name}
+                        img={contact.imgURL}
+                        tel={contact.phone}
+                        email={contact.email}
+                    />
+                )
+            })}
         </div>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,6 +7,7 @@ function Card(props) {
         <div className='card'>
             <div className='top'>
                 <h2 className='name'>{props.name}</h2>
+                <p>{props.id}</p>
                 <Avatar img={props.img} />
             </div>
             <div className='bottom'>
